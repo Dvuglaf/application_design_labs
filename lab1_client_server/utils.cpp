2912acb2045b1ad8336bc4ca862bf0015d97c2ae
Dvuglaf <aizeensan@gmail.com>
2022-09-18 16:11:50 +0400

    lab done, add utils header with description, dft and idft functions, conversions

---
+++ lab1_client_server/utils.cpp
@@ -0,0 +1,84 @@
+#include "utils.h"
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <regex>
+
+
+cv::Mat my_utils::read_image(const std::string& image_path) {
+	cv::Mat image = cv::imread(cv::samples::findFile(image_path), cv::IMREAD_COLOR);
+	if (image.empty()) {
+		throw std::invalid_argument("Could not read the image");
+	}
+	return image;
+}
+
+
+std::vector<cv::Mat> my_utils::dft(const cv::Mat& source) {
+	cv::Mat float_source;
+	source.convertTo(float_source, CV_32FC1, 1. / 255.);
+
+	cv::Mat complex_source[2] = { float_source, cv::Mat::zeros(float_source.size(), CV_32F) };
+	cv::Mat dft_input, dft_output;
+
+	merge(complex_source, 2, dft_input); //for cv::dft input (two-channel matrix instead of two matrices)
+
+	cv::dft(dft_input, dft_output, cv::DFT_COMPLEX_OUTPUT);
+
+	std::vector<cv::Mat> dft(2);
+	split(dft_output, dft);
+
+	return dft;
+}
+
+cv::Mat my_utils::idft(const cv::Mat& source) {
+	cv::Mat idft_output;
+
+	dft(source, idft_output, cv::DFT_SCALE | cv::DFT_INVERSE | cv::DFT_REAL_OUTPUT);
+
+	cv::Mat uint8_idft_output;
+	idft_output.convertTo(uint8_idft_output, CV_8U, 255);
+
+	return uint8_idft_output;
+}
+
+std::vector<std::string> my_utils::split(const std::string& string, const std::string& delimiter) {
+	std::vector<std::string> tokens;
+	size_t previous = 0, position = 0;
+	do {
+		position = string.find(delimiter, previous);
+		if (position == std::string::npos) position = string.length();
+		std::string token = string.substr(previous, position - previous);
+		if (!token.empty()) tokens.push_back(token);
+		previous = position + delimiter.length();
+	} while (position < string.length() && previous < string.length());
+	return tokens;
+}
+
+cv::Mat my_utils::mat_from_string(std::string string, const cv::MatSize& size) {
+	std::vector<double> vec;
+
+	string = std::regex_replace(string, std::regex(";"), ",");  //; -> ,
+	string = std::regex_replace(string, std::regex("[^\\d,.-]"), "");  // delete all characters except digits, ',' '.' '-'
+
+	auto splitted = my_utils::split(string, ",");
+
+	for (int i = 0; i < splitted.size(); ++i)
+		vec.push_back(std::stod(splitted[i]));
+
+	cv::Mat mat = cv::Mat::zeros(size[0], size[1], CV_64F);
+
+	for (int i = 0; i < size[0]; ++i)
+		for (int j = 0; j < size[1]; ++j)
+			mat.at<double>(i, j) = vec[j + i * 224];
+
+	return mat;
+}
+
+std::string my_utils::mat_to_string(const cv::Mat& matrix) {
+	std::string result;
+	result << matrix;
+	return result;
+}
---
+++ lab1_client_server/utils.h
@@ -0,0 +1,71 @@
+#ifndef UTILS
+#define UTILS
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <regex>
+
+
+namespace my_utils {
+
+	/**
+	 * Read image from file
+	 *
+	 * @param image_path path to image.
+	 * 
+	 * @return matrix with uint8_t values (0..255)
+	 */
+	cv::Mat read_image(const std::string& image_path);
+
+	/**
+	 * Discrete Fourier Transform on one-channel matrix
+	 *
+	 * @param source input one-channel matrix with uint8_t values (0..255).
+	 * 
+	 * @return array of two matrices: real part and imag part
+	 */
+	std::vector<cv::Mat> dft(const cv::Mat& source);
+
+	/**
+	 * Inverse Discrete Fourier Transform on two-channel matrix
+	 *
+	 * @param source input two-channel matrix: real and imag part.
+	 * 
+	 * @return one-channel matrix with uint8_t values (0..255).
+	 */
+	cv::Mat idft(const cv::Mat& source);
+
+	/**
+	 * Split string by delimiter
+	 *
+	 * @param string input string.
+	 * @param delimiter.
+	 * 
+	 * @return array of substrings, splitted by delimiter.
+	 */
+	std::vector<std::string> split(const std::string& string, const std::string& delimiter);
+
+	/**
+	 * Convert string to cv::Mat class matrix with double values
+	 *
+	 * @param string input string.
+	 * @param size output matrix size
+	 * 
+	 * @return cv::Mat class matrix with double values (64 bytes).
+	 */
+	cv::Mat mat_from_string(std::string string, const cv::MatSize& size);
+
+	/**
+	 * Convert cv::Mat class matrix to string as std::cout
+	 *
+	 * @param matrix input cv::Mat class matrix.
+	 *
+	 * @return string.
+	 */
+	std::string mat_to_string(const cv::Mat& matrix);
+
+};
+#endif 
