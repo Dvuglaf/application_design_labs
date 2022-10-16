2b07c86fd488a5d0d9d0e5bd8f0e1c8e349bfcf4
Dvuglaf <aizeensan@gmail.com>
2022-10-17 02:18:05 +0400

    add socket class

---
+++ lab3_simple_torrent/socket.cpp
@@ -0,0 +1,120 @@
+#pragma warning(disable:4996)  
+#include <iostream>
+#include <string>
+#include <WinSock2.h>
+#include <stdexcept>
+#include "socket.h"
+
+
+size_t socket_wrapper::_socket_count = 0;
+WSADATA socket_wrapper::_wsa; 
+
+socket_wrapper::socket_wrapper(int adress_family, int type, int protocol) {
+	if (_socket_count == 0) {
+		const int result = WSAStartup(MAKEWORD(2, 2), &_wsa);
+		if (result != NO_ERROR)
+			throw std::runtime_error(std::string("WSAStartup failed with error ") + std::to_string(WSAGetLastError()));
+	}
+
+	_socket = socket(adress_family, type, protocol);
+	if (_socket == INVALID_SOCKET)
+		throw std::runtime_error(std::string("socket function failed with error ") + std::to_string(WSAGetLastError()));
+
+	++_socket_count;
+}
+
+socket_wrapper::socket_wrapper(SOCKET other) : _socket(other) {}
+
+socket_wrapper::socket_wrapper(socket_wrapper&& other) noexcept {
+	_socket = other._socket;
+	other._socket = INVALID_SOCKET;
+}
+
+socket_wrapper& socket_wrapper::operator=(socket_wrapper&& other) noexcept {
+	if (other._socket == this->_socket)
+		return *this;
+
+	this->~socket_wrapper();
+	_socket = other._socket;
+	other._socket = INVALID_SOCKET;
+	return *this;
+}
+
+void socket_wrapper::bind(u_short adress_family, u_short port, const std::string& ip) const {
+	sockaddr_in socket_addr;
+
+	socket_addr.sin_family = adress_family;
+	socket_addr.sin_port = htons(port);
+	socket_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+
+	int result = ::bind(_socket, reinterpret_cast<SOCKADDR*>(&socket_addr), sizeof(socket_addr));
+	if (result == SOCKET_ERROR)
+		throw std::runtime_error(std::string("bind function failed with error ") + std::to_string(WSAGetLastError()));
+}
+
+void socket_wrapper::connect(u_short adress_family, u_short port, const std::string& ip) const {
+	sockaddr_in socket_addr;
+
+	socket_addr.sin_family = adress_family;
+	socket_addr.sin_port = htons(port);
+	socket_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+
+	const int result = ::connect(_socket, reinterpret_cast<SOCKADDR*>(&socket_addr), sizeof(socket_addr));
+	if (result == SOCKET_ERROR)
+		throw std::runtime_error(std::string("connect function failed with error ") + std::to_string(WSAGetLastError()));
+}
+
+void socket_wrapper::listen(int max_connections) const {
+	const int result = ::listen(_socket, max_connections);
+	if (result == SOCKET_ERROR)
+		throw std::runtime_error(std::string("listen function failed with error ") + std::to_string(WSAGetLastError()));
+}
+
+socket_wrapper socket_wrapper::accept() const {
+	SOCKET s = ::accept(_socket, NULL, NULL);
+
+	if (s == INVALID_SOCKET)
+		throw std::runtime_error(std::string("accept function failed with error ") + std::to_string(WSAGetLastError()));
+
+	++_socket_count;
+
+	return socket_wrapper(s);
+}
+
+int socket_wrapper::send(const char* buffer, int size) const {
+	const int result = ::send(_socket, buffer, size, 0);
+	if (result == SOCKET_ERROR)
+		throw std::runtime_error(std::string("send function failed with error ") + std::to_string(WSAGetLastError()));
+
+	return result;
+}
+
+int socket_wrapper::recv(char* buffer, int size) const {
+	int result = ::recv(_socket, buffer, size, 0);
+	if (result < 0)
+		throw std::runtime_error(std::string("recv function failed with error ") + std::to_string(WSAGetLastError()));
+
+	return result;
+}
+
+void socket_wrapper::shutdown(u_short type) const {
+	const int result = ::shutdown(_socket, type);
+	if (result == SOCKET_ERROR)
+		throw std::runtime_error(std::string("shutdown function failed with error ") + std::to_string(WSAGetLastError()));
+}
+	
+socket_wrapper::~socket_wrapper() {
+	if (_socket != INVALID_SOCKET) {
+		const int close_result = ::closesocket(_socket);
+		if (close_result != 0)
+			std::cerr << "closesocket function failed with error " << WSAGetLastError() << std::endl;
+
+		--_socket_count;
+	}
+
+	if (_socket_count == 0) {
+		const int clean_result = WSACleanup();
+		if (clean_result)
+			std::cerr << "wsacleanup function failed with error " << WSAGetLastError() << std::endl;
+	}
+}
---
+++ lab3_simple_torrent/socket.h
@@ -0,0 +1,118 @@
+#ifndef SOCKET_WRAPPER
+#define SOCKET_WRAPPER
+
+#include <WinSock2.h>
+#include <string>
+
+
+enum socket_type {
+	TCP_SOCKET = SOCK_STREAM,
+	UDP_SOCKET = SOCK_DGRAM,
+};
+
+enum address_family {
+	IPV4 = AF_INET,
+	IPV6 = AF_INET6
+};
+
+enum protocol {
+	ICMP = IPPROTO_ICMP,
+	IGMP = IPPROTO_IGMP,
+	TCP = IPPROTO_TCP,
+	UDP = IPPROTO_UDP
+};
+
+enum shutdown_type {
+	RECV = SD_RECEIVE,
+	SEND = SD_SEND,
+	BOTH = SD_BOTH
+};
+
+class socket_wrapper {
+public:
+	socket_wrapper() = delete;
+	
+	explicit socket_wrapper(SOCKET);
+
+	/**
+	* The socket function creates a socket that is bound to a specific transport service provider.
+	* 
+	* @param adress_family: address family specification (values in WinSock2.h header)
+	* @param type: the type specification for the new socket (values in WinSock2.h header)
+	* @param protocol: the protocol to be used (values in WinSock2.h header)
+	*/
+	explicit socket_wrapper(int adress_family, int type, int protocol);
+
+	socket_wrapper(const socket_wrapper&) = delete;
+	socket_wrapper& operator=(const socket_wrapper&) = delete;
+
+	socket_wrapper(socket_wrapper&&) noexcept;
+	socket_wrapper& operator=(socket_wrapper&&) noexcept;
+
+	~socket_wrapper() noexcept;
+
+	/**
+	 * The bind function associates a local address with a socket.
+	 *
+	 * @param internet_protocol: address family specification (values in WinSock2.h header)
+	 * @param port: port
+	 * @param ip: ip address
+	 */
+	void bind(u_short internet_protocol, u_short port, const std::string& ip) const ;
+
+	/**
+	* The connect function establishes a connection to a specified socket.
+	*
+	* @param internet_protocol: address family specification (values in WinSock2.h header)
+	* @param port: port
+	* @param ip: ip address
+	*/
+	void connect(u_short internet_protocol, u_short port, const std::string& ip) const;
+
+	/**
+	* The listen function places a socket in a state in which it is listening for an incoming connection.
+	* 
+	* @param max_connections: the maximum length of the queue of pending connections
+	*/
+	void listen(int max_connections) const;
+
+	/**
+	* The accept function permits an incoming connection attempt on a socket.
+	*
+	* @return a handle for the socket on which the actual connection is made
+	*/
+	socket_wrapper accept() const;
+
+	/**
+	* The send function sends data on a connected socket.
+	*
+	* @param buffer: data to send
+	* @param size: bytes of sending data
+	* 
+	* @return the total number of bytes sent
+	*/
+	int send(const char* buffer, int size) const; 
+
+	/**
+	* The recv function receives data from a connected socket or a bound connectionless socket
+	*
+	* @param buffer: a pointer to the buffer to receive the incoming data
+	* @param size: bytes to receive the incoming data
+	*
+	* @return the number of bytes received and the buffer pointed to by the buffer parameter will contain this data received
+	*/
+	int recv(char* buffer, int size) const;
+
+	/**
+	* The shutdown function disables both send and receive operations.
+	* 
+	* @param type: type of shutdown (only for receive, only for send, both)
+	*/
+	void shutdown(u_short type) const;
+
+public:
+	SOCKET _socket;
+	static WSADATA _wsa;
+	static size_t _socket_count;
+};
+#endif
