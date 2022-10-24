6d01795bf87cd0c789dcdb3477c0044c66294988
Dvuglaf <aizeensan@gmail.com>
2022-10-25 00:51:12 +0400

    processing...

--- lab3_simple_torrent/torrent.cpp
+++ lab3_simple_torrent/torrent.cpp
@@ -2,41 +2,22 @@
 #include <string>
 #include <vector>
 #include <memory>
-#include <variant>
-#include <map>
-#include "bencode.h"
 #include <fstream>
 #include <random>
-#include "SHA1.h"
-#include "utils.h"
 #include <format>
-#include <string_view>
-#include <ostream>
+#include <thread>
+#include <cpr/cpr.h>
+#include <iomanip>
+#include <bencoding.h>
+//include "bencode/BDictionary.h"
+//#include "torrent_file_parser.h"
 
 inline constexpr unsigned char operator "" _u(char arg) noexcept {
 	return static_cast<unsigned char>(arg);
 }
 
-ustring_type get_sha1_hash(const std::string& path) {
-	CSHA1 sha1;
-	sha1.HashFile(path.c_str());
-	sha1.Final();
-	unsigned char buff[20];
-	sha1.GetHash(buff);
-
-	return ustring_type(buff);
-}
-
-ustring_type read_file(const std::string& path) {
-	std::ifstream input(path, std::ios::binary);
-	ustring_type buffer(std::istreambuf_iterator<char>(input), {});
-	input.close();
-
-	return buffer;
-}
-
-ustring_type get_random_string_of_digits() {
-	ustring_type ret;
+std::string get_random_string_of_digits() {
+	std::string ret;
 
 	std::random_device r;
 	std::default_random_engine engine(r());
@@ -49,86 +30,88 @@ ustring_type get_random_string_of_digits() {
 	return ret;
 }
 
-std::string ustring_to_string(const std::basic_string<unsigned char>& str) {
-	return std::string(str.begin(), str.end());
-}
-
-void parsing_url(const ustring_type& url,
-	std::string& protocol, std::string& address, int& port, std::string& announce)
-{
-	auto split_slash = split(url, '/'_u);
-	protocol = ustring_to_string(split(split_slash[0], ':'_u)[0]);
-
-	auto address_and_port = split(split_slash[1], ':'_u);
-	address = ustring_to_string(address_and_port[0]);
-	port = std::stoi(ustring_to_string(address_and_port[1]));
-
-	if (split_slash.size() != 3) {
-		return;
-	}
-	announce = ustring_to_string(split_slash[2]);
-}
-
-std::string to_hex(const ustring_type& input) {
+std::string to_hex(unsigned char* input) {
 	std::string result;
-	for (const auto s : input) {
+	for (size_t i = 0; i < 20; ++i) {
 		char buff[3];
-		std::string tmp = itoa(static_cast<int>(s), buff, 16);
+		std::string tmp = itoa(static_cast<int>(input[i]), buff, 16);
 		if (tmp.size() == 1) {
 			tmp.insert(tmp.begin(), '0');
 		}
-		result += "%" + tmp;
+		result += tmp;
 	}
 	return result;
 }
 
-void get_request(const std::string& path) { // TODO
-	const auto file_content = read_file(path);
-	const auto data = read_bencode(file_content);
-	const auto sha1_hash = get_sha1_hash(path);
-	const auto peer_id = get_random_string_of_digits();
-
-	unsigned char key_announce[] = "announce";
-	const bencode_element elem = (std::get<dict_type>(data[0].get_value()).find(key_announce))->second;
-	if (elem.get_type() != 1) {  // not ustring_type
-		throw std::runtime_error("Wrong structure of .torrent file!");
+void decode_peer(const std::string& peer, std::string& peer_ip, std::string& peer_port) {
+	auto it = peer.begin();
+	for (; it < peer.begin() + 4; ++it) {
+		char buff[6];
+		std::cout << static_cast<int>(*it) << std::endl;
+		peer_ip += itoa(static_cast<int>(*it), buff, 16);
+		peer_ip += ".";
 	}
-	const ustring_type url = std::get<ustring_type>(elem.get_value());
+	peer_ip.pop_back();
 
-	std::string protocol, address, announce;
-	int port;
-
-	parsing_url(url, protocol, address, port, announce);
-
-	std::string header = "";
-	header += "GET ";
-	header += protocol + "://" + address;
-	if (protocol != "udp") {
-		header += "/" + announce;
+	for (; it != peer.end(); ++it) {
+		char buff[6];
+		peer_port += itoa(static_cast<int>(*it), buff, 16);
 	}
-	header += "?info_hash=" + to_hex(sha1_hash);
-	header += "&peer_id=" + to_hex(peer_id);
-	header += "&ip=0.0.0.0";
-	header += "&port=6889";
-	header += "&uploaded=0";
-	header += "&downloaded=0";
-	header += "&left=";
-	header += " HTTP/1.1";
-	header += "\r\n";
+}
+
+std::string generate_peer_id() {
+	std::string peer_id = "-UT2021-";
+	// Generate 12 random numbers
+	std::random_device rd;  // Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<> distrib(1, 9);
+	for (int i = 0; i < 12; i++)
+		peer_id += std::to_string(distrib(gen));
+	return peer_id;
+}
+
+struct request_params {
+	std::string url;
+	std::string peer_id;
+	unsigned char* info_hash;
+	unsigned long long uploaded;
+	unsigned long long downloaded;
+	unsigned long long left;
+	u_short port;
+};
+
+std::string get_peers(const request_params& params) {
+	cpr::Response res = cpr::Get(cpr::Url{ params.url }, cpr::Parameters{
+				{ "info_hash", to_hex(params.info_hash) },
+				{ "peer_id", std::string(params.peer_id) },
+				{ "port", std::to_string(params.port) },
+				{ "uploaded", std::to_string(params.uploaded) },
+				{ "downloaded", std::to_string(params.downloaded) },
+				{ "left", std::to_string(params.left) },
+				{ "compact", std::to_string(1) }
+		}, cpr::Timeout{ cpr::Timeout(15000)}
+	);
+	std::cout << res.text;
+	return res.text;
+
+}
+
+void get_peers_1(const std::string& path_to_file) {
+	try {
+		//torrent_file_parser file(path_to_file);
+		//const auto announce_url = file.get_announce();
+		//std::cout << announce_url;
+		//const auto response =  get_peers({ "", peer_id, info_hash, 0ull, 0ull, 2918443ull, 6881});
+		
+	}
+	catch (std::exception& e) {
+		std::cout << e.what();
+	}
 }
 
 int main(int argc, char* argv[]) {
-	/*
-	if (argc != 2) {
-		std::cerr << "Enter only path to torrent file!\n";
-		return 1;
-	}
-	const std::string path = argv[1];
-	*/
-	const std::string path = "\\files\\7-zip_21.01_alpha.torrent";
-	const auto file_content = read_file(path);
-	const auto data = read_bencode(file_content);
-	std::cout << data;
+	const std::string path = "C:\\Users\\aizee\\Downloads\\ComputerNetworks.torrent";
+	get_peers_1(path);
 }
 
 // Test own data
@@ -146,3 +129,16 @@ int main(int argc, char* argv[]) {
 	std::cout << "Address: \t" << address << std::endl;
 	std::cout << "Port: \t" << port << std::endl;
 	std::cout << "Announce: \t" << announce << std::endl;*/
+
+
+/*auto announce_list = std::get<list_type>(elem.get_value());
+	std::cout << announce_list;
+	for (auto& item : announce_list) {
+		std::string protocol, address, announce, port;
+
+		parsing_url(ustring_to_string(std::get<ustring_type>(std::get<list_type>(item.get_value())[0].get_value())), 
+			protocol, address, port, announce);
+		auto ip = host_to_ip(address);
+		std::cout << ip << std::endl;
+	}*/
+// TODO: проход по announce-list и попытка подключения к каждому (если не активен то ловить 10049 в connect)
