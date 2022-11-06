94333f4b267baeeb1c94f29992fa193c14d2d88f
Dvuglaf <aizeensan@gmail.com>
2022-11-07 03:03:19 +0400

    get file size, pieces hash, piece length now work correctly

--- lab3_simple_torrent/torrent_file.cpp
+++ lab3_simple_torrent/torrent_file.cpp
@@ -22,7 +22,7 @@ torrent_file::torrent_file(const bencode::data& data) : _data(data) {}
 template <typename T>
 T torrent_file::get(std::string key) const {
     const auto dict = std::get<bencode::dict>(_data);
-    return std::get<T>(dict->find(key).operator*().second);
+    return std::get<T>(dict->find(key)->second);
 }
 
 std::string torrent_file::get_info_hash() const {
@@ -36,7 +36,9 @@ std::string torrent_file::get_info_hash() const {
 }
 
 std::vector<std::string> torrent_file::split_piece_hashes() const {
-    const auto pieces = get<bencode::string>("pieces");
+    const auto info_dictionary = get<bencode::dict>("info");
+
+    const auto pieces = std::get<bencode::string>(info_dictionary->find("pieces")->second);
     if (pieces.empty())
         throw std::runtime_error("Torrent file is malformed. [File does not contain key 'pieces']");
 
@@ -51,14 +53,18 @@ std::vector<std::string> torrent_file::split_piece_hashes() const {
 }
 
 unsigned long long torrent_file::get_file_size() const {
-    const auto file_size = get<bencode::integer>("length");
+    const auto info_dictionary = get<bencode::dict>("info");
+    const auto file_size = std::get<bencode::integer>(info_dictionary->find("length")->second);
+
     if (!file_size)
         throw std::runtime_error("Torrent file is malformed. [File does not contain key 'length']");
     return file_size;
 }
 
 unsigned long long torrent_file::get_piece_length() const {
-    const auto piece_length = get<bencode::integer>("piece length");
+    const auto info_dictionary = get<bencode::dict>("info");
+    const auto piece_length = std::get<bencode::integer>(info_dictionary->find("piece length")->second);
+
     if (!piece_length)
         throw std::runtime_error("Torrent file is malformed. [File does not contain key 'piece length']");
     return piece_length;
