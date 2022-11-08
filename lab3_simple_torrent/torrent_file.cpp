0dd28e00b37474a59243a27f521de9de27161992
Dvuglaf <aizeensan@gmail.com>
2022-11-08 15:24:01 +0400

    add function add_file to decode .torrent file, change int types

--- lab3_simple_torrent/torrent_file.cpp
+++ lab3_simple_torrent/torrent_file.cpp
@@ -19,6 +19,11 @@ torrent_file::torrent_file(const std::string& path_to_file) {
 
 torrent_file::torrent_file(const bencode::data& data) : _data(data) {}
 
+void torrent_file::add_file(const std::string& path_to_file) {
+    std::ifstream file_stream(path_to_file, std::ifstream::binary);
+    _data = bencode::decode(file_stream);
+}
+
 template <typename T>
 T torrent_file::get(std::string key) const {
     const auto dict = std::get<bencode::dict>(_data);
@@ -45,33 +50,39 @@ std::vector<std::string> torrent_file::split_piece_hashes() const {
     std::vector<std::string> piece_hashes;
     assert(pieces.size() % HASH_LEN == 0);
 
-    int pieces_count = (int) pieces.size() / HASH_LEN;
+    uint32_t pieces_count = pieces.size() / HASH_LEN;
     piece_hashes.reserve(pieces_count);
-    for (int i = 0; i < pieces_count; i++)
+    for (uint32_t i = 0; i < pieces_count; i++)
         piece_hashes.push_back(pieces.substr(i * HASH_LEN, HASH_LEN));
     return piece_hashes;
 }
 
-unsigned long long torrent_file::get_file_size() const {
+uint64_t torrent_file::get_file_size() const {
     const auto info_dictionary = get<bencode::dict>("info");
+    if (info_dictionary->find("length") != info_dictionary.end()) {  // only one file with givven length
+        const auto file_size = std::get<bencode::integer>(info_dictionary->find("length")->second);
+        return file_size;
+    }
+    else {
+        throw std::runtime_error("Torrent file is malformed. [File does not contain any information about file size or more than one file]");
+    }
     const auto file_size = std::get<bencode::integer>(info_dictionary->find("length")->second);
 
-    if (!file_size)
-        throw std::runtime_error("Torrent file is malformed. [File does not contain key 'length']");
     return file_size;
 }
 
-unsigned long long torrent_file::get_piece_length() const {
+uint32_t torrent_file::get_piece_length() const {
     const auto info_dictionary = get<bencode::dict>("info");
     const auto piece_length = std::get<bencode::integer>(info_dictionary->find("piece length")->second);
 
     if (!piece_length)
         throw std::runtime_error("Torrent file is malformed. [File does not contain key 'piece length']");
-    return piece_length;
+    return static_cast<uint32_t>(piece_length);
 }
 
 std::string torrent_file::get_file_name() const {
-    const auto file_name = get<bencode::string>("name");
+    const auto info_dictionary = get<bencode::dict>("info");
+    const auto file_name = std::get<bencode::string>(info_dictionary->find("name")->second);
     if (file_name.empty())
         throw std::runtime_error("Torrent file is malformed. [File does not contain key 'name']");
     return file_name;
--- lab3_simple_torrent/torrent_file.h
+++ lab3_simple_torrent/torrent_file.h
@@ -6,10 +6,12 @@
 
 class torrent_file {
 public:
+    torrent_file() = default;
     torrent_file(const std::string&);
     torrent_file(const bencode::data&);
-    unsigned long long get_file_size() const;
-    unsigned long long get_piece_length() const;
+    void add_file(const std::string&);
+    uint64_t get_file_size() const;
+    uint32_t get_piece_length() const;
     std::string get_file_name() const;
     std::string get_announce() const;
     
