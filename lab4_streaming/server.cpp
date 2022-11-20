4d946054a8ada37650a7c84e269b6f46f6f2981c
Dvuglaf <aizeensan@gmail.com>
2022-11-20 17:15:45 +0400

    more than one client from localhost

--- lab4_streaming/server.cpp
+++ lab4_streaming/server.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <thread>
-#include <atomic>
+#include <vector>
 #include <mutex>
 #include "utils/socket/socket.h"
 
@@ -9,29 +9,33 @@ const int16_t CLIENT_PORT = 7777;
 const int16_t REPEATER_PORT = 7778;
 const std::string LOCAL_IP = "127.0.0.1";
 std::mutex mutex;
+std::vector<socket_wrapper> clients;
+
+void client_connections() {
+	while (true) {
+		const socket_wrapper master_socket_2(address_family::IPV4, socket_type::TCP_SOCKET, protocol::TCP);
+		master_socket_2.bind(address_family::IPV4, CLIENT_PORT, LOCAL_IP);
+		master_socket_2.listen(SOMAXCONN);
+
+		socket_wrapper client_socket = master_socket_2.accept();
+		std::cout << "New client connected" << std::endl;
+		mutex.lock();
+		clients.push_back(std::move(client_socket));
+		mutex.unlock();
+	}
+}
 
 int main() {
-	const socket_wrapper master_socket_1(address_family::IPV4, socket_type::TCP_SOCKET, protocol::TCP);
+	const socket_wrapper master_socket(address_family::IPV4, socket_type::TCP_SOCKET, protocol::TCP);
 
-	master_socket_1.bind(address_family::IPV4, REPEATER_PORT, LOCAL_IP);
-	std::cout << "[repeater]Bind successful" << std::endl;
+	master_socket.bind(address_family::IPV4, REPEATER_PORT, LOCAL_IP);
+	master_socket.listen(SOMAXCONN);
 
-	master_socket_1.listen(SOMAXCONN);
-	std::cout << "[repeater]Listening on socket..." << std::endl;
+	const socket_wrapper repeater_socket = master_socket.accept();
+	std::cout << "Repeater connected" << std::endl;
 
-	const socket_wrapper repeater_socket = master_socket_1.accept();
-	std::cout << "[repeater]Client connected" << std::endl;
-
-	const socket_wrapper master_socket_2(address_family::IPV4, socket_type::TCP_SOCKET, protocol::TCP);
-
-	master_socket_2.bind(address_family::IPV4, CLIENT_PORT, LOCAL_IP);
-	std::cout << "[client]Bind successful" << std::endl;
-
-	master_socket_2.listen(SOMAXCONN);
-	std::cout << "[client]Listening on socket..." << std::endl;
-
-	const socket_wrapper client_socket = master_socket_2.accept();
-	std::cout << "[client]Client connected" << std::endl;
+	std::thread clients_connect(client_connections);
+	clients_connect.detach();
 
 	while (true) {
 		int frame_size = 0;
@@ -41,8 +45,13 @@ int main() {
 		char* received = new char[frame_size];
 		received_bytes = repeater_socket.recv(received, frame_size); // frame
 		std::cout << "Received frame: " << received_bytes << " bytes\n";
-		client_socket.send((char*)&frame_size, sizeof(int));
-		client_socket.send(received, frame_size);
+		
+		mutex.lock();
+		for (const auto& client: clients) {
+			client.send((char*)&frame_size, sizeof(int));
+			client.send(received, frame_size);
+		}
+		mutex.unlock();
 		delete[] received;
 	}
 }
