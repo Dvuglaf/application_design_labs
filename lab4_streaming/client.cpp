ea21521fd26bb0763eee47b50723aef02be0fb2f
Dvuglaf <aizeensan@gmail.com>
2022-11-21 19:53:27 +0400

    add command line argument - server_ip, get rid of potential memory leaks

--- lab4_streaming/client.cpp
+++ lab4_streaming/client.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <regex>
 #include <mutex>
 #include "utils/socket/socket.h"
 #include <opencv2/opencv.hpp>
-#include <regex>
 
 const int16_t CLIENT_PORT = 7777;
-const std::string LOCAL_IP = "192.168.1.2";
+static std::string SERVER_IP;
 
 std::vector<uchar> frame_encoded;
 std::mutex mutex;
@@ -17,68 +17,69 @@ void show_frame() {
 	while (true) {
 		cv::Mat frame;
 		mutex.lock();
-		if (!frame_encoded.empty()) {
-			frame = cv::imdecode(frame_encoded, cv::IMREAD_COLOR); // decode jpeg encoded frame
-			frame_encoded.clear();
-		}
-		else {
+		if (frame_encoded.empty()) {
 			mutex.unlock();
 			continue;
 		}
+		frame = cv::imdecode(frame_encoded, cv::IMREAD_COLOR); // decode jpeg encoded frame
+		frame_encoded.clear();
 		mutex.unlock();
-		std::cout << "Decoded fram size: " << frame.size() << std::endl;
-		cv::imshow("Display window", frame);
+
+		cv::imshow("Camera", frame);
 		cv::waitKey(1);
 	}
 }
 
+int main(int argc, char* argv[]) try {
+	if (argc != 2) {
+		throw std::invalid_argument("Enter 1 argument: SERVER_IP");
+	}
+	SERVER_IP = argv[1];
 
-int main() try {
 	const socket_wrapper server_socket(address_family::IPV4, socket_type::TCP_SOCKET, protocol::TCP);
 
-	server_socket.connect(address_family::IPV4, CLIENT_PORT, LOCAL_IP);
-	std::cout << "Connect successful" << std::endl;
+	server_socket.connect(address_family::IPV4, CLIENT_PORT, SERVER_IP);
+	std::cout << "Connect to server successful" << std::endl;
 
 	std::thread show(show_frame);
 	show.detach();
 
-	server_socket.send("Hello!", 7);
+	server_socket.send("HTTP/1.0 GET", 13);
 
 	char received_header[150];
-	int received_bytes = server_socket.recv(received_header, 150); // header
+	server_socket.recv(received_header, 150);  // header
 
 	while (true) {
 		char received_length[100];
-		int received_bytes = server_socket.recv(received_length, 100); // length
-		std::cout << "Received frame size: " << received_bytes << " bytes\n";
+		int received_bytes = server_socket.recv(received_length, 100);  // length
 		std::string length_str(received_length, received_bytes);
 		std::smatch match;
 		std::regex_search(length_str, match, std::regex("\\d+"));
-		length_str = match.str(0);
-		int length = std::stoi(length_str);
-		std::cout << "Received frame size: " << length << "\n";
+
+		int length = std::stoi(match.str(0));
 
 		char* received = new char[length];
-		received_bytes = server_socket.recv(received, length); // frame
-		std::cout << "Received frame: " << received_bytes << " bytes\n";
-		if (received_bytes < length) {
-			std::cout << "\n\n\n\n" << "Failed to receive full frame. Skipping this frame ..." << "\n\n\n\n";
-			//char* lost_data = new char[length - received_bytes];
-			//received_bytes = server_socket.recv(lost_data, length - received_bytes); // lost frame
-			//delete[] lost_data;
-			delete[] received;
-			continue;
-		}
+		try {
+			received_bytes = server_socket.recv(received, length);  // frame
+			std::cout << "Received frame: " << received_bytes << " bytes" << std::endl;
+			if (received_bytes < length) {  // skip frame
+				delete[] received;
+				continue;
+			}
 
-		mutex.lock();
-		frame_encoded.reserve(received_bytes);
-		for (int i = 0; i < received_bytes; ++i) {
-			frame_encoded.push_back(received[i]);
+			mutex.lock();
+			frame_encoded.reserve(received_bytes);
+			for (int i = 0; i < received_bytes; ++i) {
+				frame_encoded.push_back(received[i]);
+			}
+			mutex.unlock();
+		} 
+		catch (std::exception&) {
+			delete[] received;
+			throw;
 		}
-		mutex.unlock();
 
 		delete[] received;
-
 	}
 }
 catch (std::exception& e) {
